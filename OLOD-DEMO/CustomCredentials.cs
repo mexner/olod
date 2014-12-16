2f1f367d7fc90d953cf710a5508c2227123707fa
mexner <mexner@users.noreply.github.com>
2014-12-16 13:58:00 -0600

    create initial environment. Has working example for searching constituents.

---
+++ OLOD-DEMO/CustomCredentials.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.ServiceModel.Description;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.Security.Cryptography;
+
+namespace OLOD_DEMO
+{
+    public class CustomCredentials : ClientCredentials
+    {
+        public CustomCredentials()
+        { }
+
+        protected CustomCredentials(CustomCredentials cc)
+            : base(cc)
+        {
+
+        }
+
+        public override System.IdentityModel.Selectors.SecurityTokenManager CreateSecurityTokenManager()
+        {
+            return new CustomSecurityTokenManager(this);
+        }
+
+        protected override ClientCredentials CloneCore()
+        {
+            return new CustomCredentials(this);
+        }
+    }
+
+    internal class CustomSecurityTokenManager : ClientCredentialsSecurityTokenManager
+    {
+        public CustomSecurityTokenManager(CustomCredentials cred)
+            : base(cred)
+        {
+
+        }
+
+        public override System.IdentityModel.Selectors.SecurityTokenSerializer CreateSecurityTokenSerializer(System.IdentityModel.Selectors.SecurityTokenVersion version)
+        {
+            return new CustomTokenSerializer(System.ServiceModel.Security.SecurityVersion.WSSecurity11);
+        }
+    }
+
+    internal class CustomTokenSerializer : WSSecurityTokenSerializer
+    {
+        public CustomTokenSerializer(System.ServiceModel.Security.SecurityVersion sv)
+            : base(sv)
+        {
+
+        }
+
+        protected override void WriteTokenCore(System.Xml.XmlWriter writer, System.IdentityModel.Tokens.SecurityToken token)
+        {
+            Random r = new Random();
+            string tokennamespace = "o";
+            DateTime created = DateTime.Now;
+            string createdStr = created.ToString("yyyy-MM-ddThh:mm:ss.fffZ");
+            string nonce = Convert.ToBase64String(Encoding.ASCII.GetBytes(SHA1Encrypt(created + r.Next().ToString())));
+            System.IdentityModel.Tokens.UserNameSecurityToken unToken = (System.IdentityModel.Tokens.UserNameSecurityToken)token;
+            writer.WriteRaw(String.Format(
+                "<{0}:UsernameToken u:Id=\"" + token.Id + "\" xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">" +
+                "<{0}:Username>" + unToken.UserName + "</{0}:Username>" +
+                "<{0}:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">" +
+                unToken.Password + "</{0}:Password>" +
+                "<{0}:Nonce EncodingType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary\">" +
+                nonce + "</{0}:Nonce>" +
+                "<u:Created>" + createdStr + "</u:Created></{0}:UsernameToken>", tokennamespace));
+        }
+
+        protected String ByteArrayToString(byte[] inputArray)
+        {
+            StringBuilder output = new StringBuilder("");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output.Append(inputArray[i].ToString("X2"));
+            }
+            return output.ToString();
+        }
+        protected String SHA1Encrypt(String phrase)
+        {
+            UTF8Encoding encoder = new UTF8Encoding();
+            SHA1CryptoServiceProvider sha1Hasher = new SHA1CryptoServiceProvider();
+            byte[] hashedDataBytes = sha1Hasher.ComputeHash(encoder.GetBytes(phrase));
+            return ByteArrayToString(hashedDataBytes);
+        }
+    }
+}
---
+++ OLOD-DEMO/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using OLOD_DEMO.OLODWebService;
+
+namespace OLOD_DEMO
+{
+    class Program
+    {
+        protected static OrangeLeapClient _client;
+        private static string _apiUsername = ConfigurationSettings.AppSettings["api_username"];
+        private static string _apiPassword = ConfigurationSettings.AppSettings["api_password"];
+        
+        public static void Main(string[] args)
+        {
+            _client = new OrangeLeapClient();
+            EstablishConnection();
+            FindConstituent();
+        }
+
+        protected static List<constituent> FindConstituent()
+        {
+            var results = new List<constituent>();
+
+            var request = new FindConstituentsRequest();
+            request.firstName = "chris";
+            request.lastName = "mechsner";
+
+            //VALIDATION ERROR
+            //request.primaryEmail = new email() { emailAddress = "chris@ascendio.com", primary = true};
+            //request.primaryPhone = new phone() { number = "123-123-1234", primary = true};
+
+            //VALIDATION ERROR
+            //request.primaryAddress = new address
+            //                                 {
+            //                                     postalCode = 75019,
+            //                                     addressLine1 = "123 Test Main St.",
+            //                                     city = "Dallas",
+            //                                 };
+            
+            try
+            {
+                var response = _client.FindConstituents(request);
+
+                //only grab active accounts
+                response = response.Where(x => !x.deleted && !x.inactive).ToArray();
+
+                if (response.Any())
+                {
+                    results.AddRange(response);
+                }
+            }
+            catch (System.ServiceModel.FaultException exc)
+            {
+                var txt = exc;
+            }
+            catch (System.Web.Services.Protocols.SoapException exception)
+            {
+                var y = exception;
+            }
+            catch (Exception ex)
+            {
+                var x = ex;
+
+            }
+
+            return results;
+        }
+
+        protected static void EstablishConnection()
+        {
+            _client.ChannelFactory.Endpoint.Behaviors.Remove<System.ServiceModel.Description.ClientCredentials>();
+            _client.ChannelFactory.Endpoint.Behaviors.Add(new CustomCredentials());
+
+            if (_client.ClientCredentials != null)
+            {
+                _client.ClientCredentials.UserName.UserName = _apiUsername;
+                _client.ClientCredentials.UserName.Password = _apiPassword;
+            }
+        }
+    }
+}
